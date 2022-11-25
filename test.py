8d2509c003d2803ea529cefcc90591fe29354b21
jalal776 <jalal.stud@gmail.com>
2022-11-25 12:57:03 +0330

    requirments
    created requirments.txt

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-import numpy as np
 
-def test_args(a,*args):
-    print(args)
-    
+l1=['Ali','reza','omid']
+l2=[30,12,18]
+l3=[18.20,15.50,12.0]
 
-test_args(2)
-test_args(1,2)
-test_args(1,2,3,4)
-test_args(*[1,2,3])
+print(zip(l1,l2,l3))
+
+for name,age,avg in zip(l1,l2,l3):
+    print("{0},{1},{2}".format(name,age,avg))
+    # print(f"{name},{age},{avg}")

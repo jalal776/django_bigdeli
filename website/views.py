bc6322f38380a189860b1fe2e2ee6bcbecd12d0a
jalal <jalal.stud@gmail.com>
2024-01-31 18:33:04 +0330

    adding templates folder
    1. adding templates folder in website App
    2. creating a folder named website in templates
    3.adding 3 html files into website named: index, contact and about

--- website/views.py
+++ website/views.py
@@ -3,15 +3,18 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def contact(request):
-    return HttpResponse("<h1>Contact</h1>")
+    # return HttpResponse("<h1>Contact</h1>")
+    return render(request, 'website/contacts.html')
 
 def about(request):
-    return HttpResponse("<h1>About</h1>")
+    # return HttpResponse("<h1>About</h1>")
+    return render(request, 'website/about.html')
 
 # def json_test(request):
 #     return JsonResponse({'name':'Jalal'})
 
 def index_view(request):
-    return HttpResponse('<h1>Index Page</h1>')
+    # return HttpResponse('<h1>Index Page</h1>')
+    return render(request, 'website/index.html')
 
 

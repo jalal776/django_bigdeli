209252e07a307ecec3836fa08c6a6e3349e6ab45
jalal <jalal.stud@gmail.com>
2024-02-05 20:03:11 +0330

    adding statics
    - adding statics folder with simple css file
    -modify settings.py:  adding
        STSTIC_ROOT and STATIC_URL  and
        MEDIA_URL and MEDIA_ROOT and
         STATICFILES_DIRS
    -modifying mysite/urls with new
       urlpatterns[]
    
    -using jinja to load statics in index.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -119,6 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

7bad9ddec756974dad708912976ec260f4739ead
jalal <jalal.stud@gmail.com>
2024-02-07 22:09:26 +0330

    making dynamic href
    - in urls.py in website app: creating
         app_name= 'website'
    
    -making dynamic
       href= {% url 'website:index'%} for
       index, about and contacts

--- website/urls.py
+++ website/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 from website.views import index_view,contact, about
 
+app_name='website'
 urlpatterns = [
-    path('contact',contact),
-    path('about',about),
-    path('',index_view)
+    path('contact',contact,name='contact'),
+    path('about',about,name='about'),
+    path('',index_view,name='index')
     
 ]

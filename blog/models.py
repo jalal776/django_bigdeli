2af1c7921b26f031848ccc362385c9b3207c24c6
jalal <jalal.stud@gmail.com>
2024-02-17 23:18:35 +0330

    creating post and contact DB
    - write post class in blog:models.py
    - write contact class in website: models.py
    
    - run manage,py  makemigrations  and migrate

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    # image = 
+    # tag = 
+    # author = 
+    # category = 
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    counted_views = models.IntegerField(default = 0)
+    status = models.BooleanField()
+    published_date = models.DateTimeField(null = True)
+    created_date = models.DateTimeField(auto_now_add = True)
+    upated_date = models.DateTimeField(auto_now_add= True)    
--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length = 255)
+    messsagw = models.TextField()
+    created_date = models.DateTimeField(auto_now_add = True)
+    upated_date = models.DateTimeField(auto_now= True)
+    
+    
